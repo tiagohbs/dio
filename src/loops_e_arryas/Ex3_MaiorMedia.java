8fba4bbd9a8702a60824146535091d22e2a9fde1
tiagohbs <tiagoborges2940@gmail.com>
2022-05-06 00:30:19 -0300

    Programando no trampo

--- src/loops_e_arryas/Ex3_MaiorMedia.java
+++ src/loops_e_arryas/Ex3_MaiorMedia.java
@@ -5,17 +5,21 @@ import java.util.Scanner;
 public class Ex3_MaiorMedia {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int numero;
         int count = 0;
-
+        int maior = 0;
+        float soma = 0;
+        int max;
+        System.out.println("Quantos números vc ira inserir? ");
+        max = scan.nextInt();
         do {
             System.out.println("Número: ");
             numero = scan.nextInt();
+            if (numero > maior) maior = numero;
+            soma = numero + soma;
             count++;
-
-        } while(count < 5) ;
-
-
+        } while(count < max);
+        System.out.println("O maior número foi o " + maior);
+        System.out.println("Media: "+ soma/max);
     }
 }
