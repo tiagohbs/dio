2243ab4bf8764f782407fa9d5131aefc8d131eda
tiagohbs <tiagoborges2940@gmail.com>
2022-05-06 02:25:52 -0300

    Programando no trampo

--- src/loops_e_arryas/Ex4_ParImpar.java
+++ src/loops_e_arryas/Ex4_ParImpar.java
@@ -10,14 +10,14 @@ public class Ex4_ParImpar {
         int count = 0; // sempre iniciar variável contadora de ciclos.
         int max;
         int num;
-        System.out.println("Quantos números inteios usaremos? ");
-        max = scan.nextInt(); // similar ao comando leia em C#, aqui ele ler o que é digitado e salva na variavel "max"
+        System.out.println("Quantos números inteiros usaremos? ");
+        max = scan.nextInt(); // similar ao comando leia em C#, aqui ele ler o que é digitado e salva na variável "max"
         do{
             System.out.println("Numero: ");
             num = scan.nextInt();
             if (num % 2 == 0 ) par++; // Aqui acontece a conferencia se o resto da divisão por 2 é igual a zero, se for "par" recebe +1
             else impar++; // aqui é o contrario do par, depois da negativa impar recebe +1.
-            count++;// contador que adiciona +1 a  cada ciclo de execução. Que esta limitado pela pergunta inicial do codigo.
+            count++;// contador que adiciona +1 a  cada ciclo de execução. Que esta limitado pela pergunta inicial do código.
         } while (count < max); // limitador de ciclos
         System.out.println("Impares: "+ impar);
         System.out.println("Pares: "  + par);
