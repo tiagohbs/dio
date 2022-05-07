bf13214f8a542f99995d53bc6bbddefff234499c
Tiago <tiagoborges2940@gmail.com>
2022-05-07 00:03:14 -0300

    tabuada e fatorial on

---
+++ src/loops_e_arryas/Ex6_Fatorial.java
@@ -0,0 +1,23 @@
+package loops_e_arryas;
+
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num;
+        int max=1;
+
+        System.out.println("Digite um numero Inteiro para fatorar: ");
+        num = scan.nextInt();
+        System.out.println("Fatorial do número: " + num);
+        for (int count = num ;count >=1; count--){
+            //System.out.println(num +" X " + (count-1)+" = " + " " +  num * count );
+
+            max = max * count;
+
+
+        }
+        System.out.println("Resultado: " + max);
+    }
+}
